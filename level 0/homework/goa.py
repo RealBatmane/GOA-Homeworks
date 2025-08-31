4404ff794610af31e49cf3b70547e59615d7fba4
RealBatmane <nikalursmanadze@gmail.com>
2025-08-31 10:54:40 -0700

    GOA BEST

--- level 0/homework/goa.py
+++ level 0/homework/goa.py
@@ -1,12 +1,83 @@
 from turtle import *
-speed(50)
-# width("7")
-color("yellow")
+width("7")
+speed(30)
+color("black")
 shape("turtle")
+
 
 
 forward(200)
 left(90)
 
 forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90) 
+
+
+
+forward (70)
+color("black")
+begin_fill()
+left(90)
+forward (120) 
 right(90)
+forward(60)
+right(90)
+forward(120)
+end_fill()
+
+penup()
+goto(200,200)
+pendown()
+color("black")
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+
+
+
+penup()
+goto(137,130)
+pendown()
+color("black")
+begin_fill()
+left(120)
+forward(45)
+left(90)
+forward(57)
+left(90)
+forward(45)
+left(90)
+forward(57)
+end_fill()
+
+
+penup()
+goto(14,130)
+pendown()
+color("black")
+begin_fill()
+left(90)
+forward(45) 
+left(90)
+forward(45)
+left(90)
+forward(45)
+left(90)
+forward(45)
+end_fill()
+
+
+penup()
+goto(200,200)
+pendown()
+color("black")
+exitonclick()

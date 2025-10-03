0d775ca5600acb95ae368696fbeb5c7dc722f7ee
RealBatmane <nikalursmanadze@gmail.com>
2025-10-03 12:53:13 -0700

    GOA BEST

---
+++ level 2/classwork.py
@@ -0,0 +1,28 @@
+
+# string (str) -> სტრინგი ანუ ნიზმი რომელშიც მოთავსებულია ბრჭყალებში
+
+# integer (int) -> ინტეგერი ანუ მთელი რიცხვი
+
+# float -> ანუ ნამდვილი ათწილადი რიცხვი
+
+# boolean (bool) -> ორი შესაძლებელი მნიშვნელობიდან ერთი-ერთად True ან False
+
+# es aris string
+#1
+name1 ="nikolozi"
+
+# integer 
+name2 =5
+
+#float
+name3 =5.3
+
+#2
+age=15
+
+print(age)
+
+#3
+name4='giorgi' 
+
+print(name4)
---
+++ level 2/homework.py
@@ -0,0 +1,35 @@
+
+# davaleba 1
+name = "nikolozi"
+
+print(name)
+
+Lastname = "lursmanashvili" 
+
+print(Lastname)
+
+# davaleba 2
+name1 = 5 
+ 
+print(name)
+
+name2 = "nikolozi" 
+
+print(name2) 
+
+name3 = 2.5 
+
+print (name3)
+
+is_student = True 
+
+print(is_student)
+
+# davaleba 4
+dad_age = 44 
+
+grandma_age = 65
+
+grandpa_age =76 
+
+print(dad_age + grandma_age + grandpa_age)

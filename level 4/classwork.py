eee5cfcb91539af0933f5cf46ed9a8bc0325aef9
RealBatmane <nikalursmanadze@gmail.com>
2025-10-09 05:20:45 -0700

    GOA BEST

---
+++ level 4/classwork.py
@@ -0,0 +1,42 @@
+#monacemta tipebia 
+# string=სტრინგი არის ნებისმიერი მონაცემი რომელიც არის მონიშნული ბრწყალებში
+# integer= ნებისმიერი მთელი რიცვხი
+# float=ნებსმიერი ათწილადი რიცვხი
+# boolean= ორი მნისვნელობიa  რაც შეიძლება იყოს true ან false
+# კონკატინაცია= სტრინგების დაკავშირება
+# ცვლადის განახლება= ახალი ცვლადის შემოტანა და  ანუ მისი განახლება
+
+# string ამ შემთხვევაში ნიკოლოზია სტრინგი
+# name = "nikoloz" 
+
+# integer
+# number1 = 5 
+
+# float
+# number2 = 2.5 
+
+# is_student = True
+
+# კონკატინაცია
+
+# name2 = "nikolozi"
+# lastname = "lursmanashvili"
+
+# print("nikolozi",+"lursmanashvili")
+
+# ცვლადის განახლება
+
+# name = "nikolozi"
+
+# name = "giorgi"
+
+# print(name)
+
+temperature = 1
+temperature = 2
+temperature = 3
+print(temperature)
+
+number1 = 15
+number2 = 10 
+print(number1  + number2)
---
+++ level 4/homework.py
@@ -0,0 +1,47 @@
+
+# davaleba 1)
+points = 50
+points = 100
+points = 150
+print(points)
+
+# davaleba 2) shecdomit
+
+name = "nikolozi" 
+name = "aleqsandre"
+name = dachi
+number = 10
+number = 50 
+number = 60 
+number = 80 
+number = 90
+number = 100
+print(name + " " + str(number))
+
+# swori versia
+name = "nikolozi" 
+name = "aleqsandre"
+name = "dachi"
+number = 10                                                 #str internetidan movidzine dasaxmareblad
+number = 50 
+number = 60 
+number = 80 
+number = 90
+number = 100
+print(name + " " + str(number))
+
+# davaleba 3)
+number = 69
+age = 15
+result = number + age
+print (result)
+
+
+# davaleba 4)
+print(5+10)
+print(5*10)
+print(5/10)
+print(5//10)
+print(5%10)
+
+

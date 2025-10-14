6c4d1a1247888bd5b42ce729958a56dee0698f60
RealBatmane <nikalursmanadze@gmail.com>
2025-10-14 04:57:46 -0700

    goa best

---
+++ level 6/homework.py
@@ -0,0 +1,19 @@
+
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+print(int(num1) + int(num2))
+
+
+user_name = input("enter your name : ")
+user_lastname = input("enter your lastname: ")
+user_age = input("enter your age : ")
+user_favcar = input("enter your favorite car: ")
+print("my name is ", user_name, user_lastname, "i am ",user_age, "years old and my favorite car is ", user_favcar)
+
+
+num3 = int(input("enter first number"))
+num4 = int(input ("enter second number"))
+print(num3 % num4)
+
+best_academy = input("best fav programing academy")
+print("my favorite academy is",best_academy,)

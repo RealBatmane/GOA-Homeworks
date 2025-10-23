798b834221e5953ad6ae55c8fa50c2f35a7dce26
RealBatmane <nikalursmanadze@gmail.com>
2025-10-23 08:41:18 -0700

    GOA BEST

---
+++ level 8/classwork/classwork.py
@@ -0,0 +1,21 @@
+
+age = input("enter your age")
+print(type(age))
+
+number_int = int(input("enter number 1:"))
+print(number_int)
+
+number2 = float(input("enter number 2:"))
+print(number2)
+
+is_student = bool(input("enter anwer:"))
+print(is_student)
+
+name = input("enter your name :")
+lastname = input("enter your lastname :")
+age = input("enter your age :")
+haight = input("enter your haight :")
+print(type(name))
+print(type(lastname))
+print(type(age))
+print(type(haight))
---
+++ level 8/homework/homework.py
@@ -0,0 +1,25 @@
+
+#  1)შემოიტანეთ თქვენი ოჯახის წევრების ასაკი და დაუპრინტეთ თუ რამდენი წლისები იქნებიან 25 წლის შემდეგ(გამოიყენეთ int ფუნქცია).
+age = int(input("ojaxis wevri pirveli :"))
+age2 = int(input("ojaxis wevri meore :"))
+print (age + age2 + 25)
+
+
+#  2)მომხმარებელს შემოატანინეთ მისი სახელი და type ფუნქციის საშუალებით გაიგეთ რა ტიპის მონაცემია შემოტანილი მნიშვნელობა.
+
+name = input("enter your name :")
+print(type(name))
+
+# 3)მომხმარებელს შემოატანინეთ მისი ასაკი და შემდეგ გადააქციეთ სტრინგად
+
+age3 = input("enter your age :")
+print (str(age3))
+
+#  4)კომენტარებით ახსენით რას აკეთებს str(),int() და float() ფუნქციები.
+# str()- aqcevs shignit myof monacems stringad anu "",int()-meshveobit shignit myofi nebismieri mteli ricvxi gaxdeba,float()- meshveobit shignit myofi atwiladad chaiwereba
+
+
+
+
+
+
